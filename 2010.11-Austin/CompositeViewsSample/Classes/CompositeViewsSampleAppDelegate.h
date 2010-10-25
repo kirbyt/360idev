80177f3112c15a99f89e0c1f900f6beb1d55a0fe
Kirby Turner <kirby@thecave.com>
2010-10-25 16:34:27 -0400

    Initial project import.

---
+++ 2010.11-Austin/CompositeViewsSample/Classes/CompositeViewsSampleAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  CompositeViewsSampleAppDelegate.h
+//  CompositeViewsSample
+//
+//  Created by Kirby Turner on 10/25/10.
+//  Copyright 2010 White Peak Software Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CompositeViewsSampleAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ 2010.11-Austin/CompositeViewsSample/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CompositeViewsSample
+//
+//  Created by Kirby Turner on 10/25/10.
+//  Copyright 2010 White Peak Software Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
