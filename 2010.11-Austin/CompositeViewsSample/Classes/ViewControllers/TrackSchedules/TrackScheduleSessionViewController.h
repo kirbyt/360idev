935480e82922fda72b270056ca589f8e4a47f380
Kirby Turner <kirby@thecave.com>
2010-11-02 12:52:40 -0400

    Work on the session details.

---
+++ 2010.11-Austin/CompositeViewsSample/Classes/ViewControllers/TrackSchedules/TrackScheduleSessionViewController.h
@@ -0,0 +1,17 @@
+//
+//  TrackScheduleSessionViewController.h
+//  CompositeViewsSample
+//
+//  Created by Kirby Turner on 11/2/10.
+//  Copyright 2010 White Peak Software Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TrackScheduleSessionViewController : UIViewController 
+{
+
+}
+
+@end
---
+++ 2010.11-Austin/CompositeViewsSample/Classes/ViewControllers/TrackSchedules/TrackScheduleSessionViewController.m
@@ -0,0 +1,14 @@
+//
+//  TrackScheduleSessionViewController.m
+//  CompositeViewsSample
+//
+//  Created by Kirby Turner on 11/2/10.
+//  Copyright 2010 White Peak Software Inc. All rights reserved.
+//
+
+#import "TrackScheduleSessionViewController.h"
+
+
+@implementation TrackScheduleSessionViewController
+
+@end
